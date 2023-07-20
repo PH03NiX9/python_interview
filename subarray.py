3de2fe10b8a0ac3bbba7aa1f2754c54616cdef91
zpetracek <68917648+PH03NiX9@users.noreply.github.com>
2023-07-20 10:51:29 +0300

    Update subarray.py

--- subarray.py
+++ subarray.py
@@ -1,4 +1,5 @@
-# Given an integer array nums, return the number of subarrays filled with 0. A subarray is a contiguous non-empty sequence of elements within an array.
+# Given an integer array nums, return the number of subarrays filled with 0.
+# A subarray is a continuous, non-empty sequence of elements within an array.
 from ast import List
 
 
