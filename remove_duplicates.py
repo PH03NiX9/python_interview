c7914c130aedf3b39f8c339dd28ec548c659b785
zpetracek <68917648+PH03NiX9@users.noreply.github.com>
2023-07-20 10:52:30 +0300

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,4 +1,5 @@
-# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.
+# Given an integer array nums sorted in non-decreasing order
+# remove the duplicates in-place such that each unique element appears only once.
 from ast import List
 
 
